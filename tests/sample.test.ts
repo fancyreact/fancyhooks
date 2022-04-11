c71740364005d993ff9d31a719fc2245100135db
Adel Armand <adel.armand@hotmail.com>
2022-04-11 22:58:45 +0430

    Remove unnecessary 'test' directory

--- tests/sample.test.ts
+++
@@ -1,9 +0,0 @@
-describe('sample test', () => {
-  test('should pass the test', () => (
-    expect(1 + 1).toEqual(2)
-  ));
-
-  test('should not pass the test', () => (
-    expect(1 + 1 === 3).toBeFalsy()
-  ));
-});
